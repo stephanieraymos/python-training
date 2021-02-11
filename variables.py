e6990848987a0795b3313e5302c178e95ae9ff7f
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-02-10 22:08:29 -0800

    Job added

--- variables.py
+++ variables.py
@@ -1,4 +1,6 @@
 name = "Stephanie Raymos"
 age = 28
+job = "Software Engineer."
 
-print(name + " is " + str(age) + " years old!")
+print(name + " is " + str(age) + " years old.")
+print(name + " is a " + job)
