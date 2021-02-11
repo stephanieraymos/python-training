be13dba75a739f2b627e057dfa790563c30ddbd0
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-02-10 22:15:05 -0800

    Using functions together

--- strings.py
+++ strings.py
@@ -1,5 +1,8 @@
-print("This is a \nstring.") #new line
-print("This is a \"string.") #literal quotation 
+# print("This is a \nstring.") #new line
+# print("This is a \"string.") #literal quotation 
 
-phrase = "This is a phrase, "
-print(phrase + "and it is cool.")
+# phrase = "This is a phrase, "
+# print(phrase + "and it is cool.")
+
+phrase = "This is another phrase"
+print(phrase.upper().isupper())
