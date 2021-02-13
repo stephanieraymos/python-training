99a7d8f0bd6ef46d60b5ce7eb60799d289aad65f
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-02-12 20:16:24 -0800

    Floor and ceiling functions

--- numbers.py
+++ numbers.py
@@ -1,7 +1,12 @@
+from math import * #Math functions imported
+
 # number = 5 
 # string = " is my number"
 # print(str(number) + string)
 
 # print(pow(3,2))
 
-print(round(3.2))
+# print(round(3.2))
+
+print(floor(3.7))
+print(ceil(3.7))
