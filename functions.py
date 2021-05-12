855121f005046912b87e17c1bc6c18348109a0f5
Stephanie Raymos <stephanieraymos@gmail.com>
2021-05-12 15:48:11 -0700

    Put validation logic into function

--- functions.py
+++ functions.py
@@ -9,12 +9,15 @@ def daysToUnits(days):
         return "You entered 0, please enter a number greater than 0 to continue with conversion"
 
 
-userInput = input("Enter a number of days and I will convert it to hours! \n")
+def validateAndExecute():
+    if userInput.isdigit():
+        userInputToNum = int(userInput)
+        calculatedValue = daysToUnits(userInputToNum)
+        print(calculatedValue)
+    else:
+        print("Your input is not a valid number, stop trying to trick the system.")
 
-if userInput.isdigit():
-    userInputToNum = int(userInput)
-    calculatedValue = daysToUnits(userInputToNum)
-    print(calculatedValue)
-else:
-    print("Your input is not a valid number, stop trying to trick the system.")
+
+userInput = input("Enter a number of days and I will convert it to hours! \n")
+validateAndExecute()
 
