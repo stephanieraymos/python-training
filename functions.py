831ae98b41ec700fd661c3d2fe7a548e071a417a
Stephanie Raymos <stephanieraymos@gmail.com>
2021-05-12 13:43:49 -0700

    Taking input from user and using it within application

--- functions.py
+++ functions.py
@@ -2,10 +2,11 @@ toUnits = 24
 nameOfUnit = "hours"
 
 
-def daysToUnits(days, customMessage):
-    print(f"{days} days are {days * toUnits} {nameOfUnit}")
-    print(customMessage)
+def daysToUnits(days):
+    return f"{days} days are {days * toUnits} {nameOfUnit}"
 
 
-input("First input")
-input("Second input")
+userInput = input("Enter a number of days and I will convert it to hours! \n")
+userInputToNum = int(userInput)
+calculatedValue = daysToUnits(userInputToNum)
+print(calculatedValue)
