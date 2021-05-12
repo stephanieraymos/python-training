9e09a100c1a0fb739ad6bc5eb483eaeb7a91f6c1
Stephanie Raymos <stephanieraymos@gmail.com>
2021-05-12 11:30:50 -0700

    Fixed venv

---
+++ functions.py
@@ -0,0 +1,9 @@
+toUnits = 24
+nameOfUnit = "hours"
+
+
+def daysToUnits(days):
+    print(f"{days} days are {days * toUnits} {nameOfUnit}")
+
+
+daysToUnits(1)
--- variables.py
+++ variables.py
@@ -4,3 +4,12 @@ job = "Software Engineer."
 
 print(name + " is " + str(age) + " years old.")
 print(name + " is a " + job)
+
+
+toUnits = 24 * 60 * 60
+nameOfUnit = "seconds"
+
+print(f"20 days are {20 * toUnits} {nameOfUnit}")
+print(f"30 days are {30 * toUnits} {nameOfUnit}")
+print(f"50 days are {50 * toUnits} {nameOfUnit}")
+print(f"100 days are {100 * toUnits} {nameOfUnit}")
