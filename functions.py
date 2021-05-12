0f29c307e9e9415c5a2ec75632ee48b3b4cbf73b
Stephanie Raymos <stephanieraymos@gmail.com>
2021-05-12 14:39:39 -0700

    Type checking

--- functions.py
+++ functions.py
@@ -3,10 +3,13 @@ nameOfUnit = "hours"
 
 
 def daysToUnits(days):
+    conditionalCheck = days > 0
+    print(type(conditionalCheck))
     if days > 0:
         return f"{days} days are {days * toUnits} {nameOfUnit}"
     else:
         return "You entered a negative number, please enter a number greater than 0 to continue with conversion."
+
 
 userInput = input("Enter a number of days and I will convert it to hours! \n")
 userInputToNum = int(userInput)
